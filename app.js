12f6e081b17b0d0dee780799a868a27d1c3b5bb4
Alaev Evgeny <alaev89@yandex-team.ru>
2019-12-31 18:12:46 +0100

    fix app.js

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
-const express = require('express');
+const express = require('express')
 const config = require('config');
-const mongoose = require('mongoose');
 const path = require('path');
-
+const mongoose = require('mongoose');
 
 const app = express();
-const PORT = config.get('port') || 5000;
 
-app.use(express.json({extended: true}));
+app.use(express.json({ extended: true }));
 
 app.use('/api/auth', require('./routes/auth.routes'));
 app.use('/api/link', require('./routes/link.routes'));
@@ -15,22 +13,24 @@ app.use('/t', require('./routes/redirect.routes'));
 
 if (process.env.NODE_ENV === 'production') {
     app.use('/', express.static(path.join(__dirname, 'client', 'build')));
+
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
+
+const PORT = config.get('port') || 5000
 
 async function start() {
     try {
         await mongoose.connect(config.get('mongoUri'), {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-            useCreateIndex: true,
+            useCreateIndex: true
         });
-
-        app.listen(PORT, () => console.log(`app has been started on port ${PORT}`));
+        app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`));
     } catch (e) {
-        console.error('Server error:', e);
+        console.log('Server Error', e.message);
         process.exit(1);
     }
 }
